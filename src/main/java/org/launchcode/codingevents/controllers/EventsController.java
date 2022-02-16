9a2bcd5ab46fc9a2b08d479766c195f10407ff6a
1mbalducci <michelle.balducci@gmail.com>
2022-02-15 20:32:31 -0600

    fragments

--- src/main/java/org/launchcode/codingevents/controllers/EventsController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventsController.java
@@ -3,7 +3,9 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,15 +13,32 @@ import java.util.List;
 @Controller
 @RequestMapping("events")
 public class EventsController {
+    private static List<String> events = new ArrayList<>();
 
     @GetMapping
     public String displayAllEvents(Model model){
-        List<String> events = new ArrayList<>();
-        events.add("Code with Pride");
-        events.add("Strange Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");
-        model.addAttribute("events," events);
+        model.addAttribute("events", events);
         return "events/index";
     }
+
+    //lives at /events/create
+    @GetMapping("create")
+    public String renderCreateEventForm(){
+        return "events/create";
+    }
+
+    //lives at /events/create. it can be names the same as the method about it because they handle different types of request
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName )
+    {
+    events.add(eventName);
+    //redirects people to the root path of the specific controler
+    return "redirect:";
+    }
+
+
+
+
+
+
 }
