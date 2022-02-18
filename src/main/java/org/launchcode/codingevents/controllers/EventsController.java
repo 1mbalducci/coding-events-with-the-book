a65818e0a9807cfef6caf6b13a7f071be1885698
1mbalducci <michelle.balducci@gmail.com>
2022-02-18 12:31:25 -0600

    working with Errors

--- src/main/java/org/launchcode/codingevents/controllers/EventsController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventsController.java
@@ -1,11 +1,10 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.Models.Event;
+import org.launchcode.codingevents.data.EventData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,32 +12,55 @@ import java.util.List;
 @Controller
 @RequestMapping("events")
 public class EventsController {
-    private static List<String> events = new ArrayList<>();
 
     @GetMapping
     public String displayAllEvents(Model model){
-        model.addAttribute("events", events);
+        model.addAttribute("title","All Events");
+        model.addAttribute("events", EventData.getAll());
         return "events/events/index";
     }
 
     //lives at /events/create
     @GetMapping("create")
-    public String renderCreateEventForm(){
+    public String displayCreateEventForm(Model model){
+        model.addAttribute("title","Create Event");
+        model.addAttribute(new Event());
         return "events/events/create";
     }
 
     //lives at /events/create. it can be names the same as the method about it because they handle different types of request
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName )
+    //@ModelAttribute allows you to not have to put in tons of @RequestParam's
+    //instead you can just reference the models/event file and it will take those parameters
+    public String processCreateEventForm(@ModelAttribute @Valid Event newEvent, Errors errors, Model model)
     {
-    events.add(eventName);
+        if (errors.hasErros()) {
+            model.addAttribute("title","Create Event");
+            model.addAttribute("errorMsg","Bad data!");
+            return "events/events/create";
+
+
+        }
+    EventData.add(newEvent);
     //redirects people to the root path of the specific controler
     return "redirect:";
     }
 
+    @GetMapping("delete")
+    public String displayDeleteEventForm (Model model){
+        model.addAttribute("title","Delete Events");
+        model.addAttribute("events",EventData.getAll());
+        return "events/delete";
+    }
 
+    @PostMapping("delete")
+    public String processDeleteEventsForm(@RequestParam(required = false) int[] eventIds) {
 
-
-
-
+        if (eventIds != null) {
+            for (int id : eventIds) {
+                EventData.remove(id);
+            }
+        }
+        return "redirect:";
+    }
 }
